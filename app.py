3347375a83cb568c755e5e21addc60f3b27e5eca
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-02-15 19:16:03 -0600

    The app reads the contents of the media directory and uses it to populate the list. media titles and urls are redirected on window load. Adds small margin between radio buttons.

--- app.py
+++ app.py
@@ -12,22 +12,26 @@ This application is meant to be run by uwsgi. It handles requests by returning a
 """
 
 import jinja2
+import os
 
 
 def application(env, start_response):
 
-    media = ['rectify.mp4', 'test.txt', 'watchtower.mp3', 'xkcd.png']
+    MEDIA_PATH = "/home/abid/scripts/chrome_cast/local-chromecast/media"
+
+    media = [f for f in os.listdir(MEDIA_PATH) if os.path.isfile( os.path.join(MEDIA_PATH, f) )]        # Get a (alphabetically) sorted list of all files (not folders) in the media folder
+    media.sort()
 
     start_response('200 OK', [('Content-Type', 'text/html')])
 
     TEMPLATE_FILE = "/home/abid/scripts/chrome_cast/local-chromecast/index.html"        # Absolute Path to template
-    MEDIA_PATH = "http://192.168.1.10:3435/media/"
+    MEDIA_URL_PATH = "http://192.168.1.10:3435/media/"
 
     templateLoader = jinja2.FileSystemLoader("/")        # We will be serving a template from the same folder as this script
     templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)       # We pass in information for removing extraneous whitespace from the rendered text
 
     template = templateEnv.get_template(TEMPLATE_FILE)
 
-    output = template.render({"media": media, "media_path": MEDIA_PATH})
+    output = template.render({"media": media, "media_path": MEDIA_URL_PATH})
 
     return str(output)      # The output has to be explicitly cast as a string for this to work
